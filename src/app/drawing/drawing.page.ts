21be0e91337db3c31bfb0eed256ff97de50130cf
Umi <julianogrady07@gmail.com>
2022-05-18 21:49:06 -0400

    formatting

--- src/app/drawing/drawing.page.ts
+++ src/app/drawing/drawing.page.ts
@@ -6,65 +6,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drawing.page.scss'],
 })
 export class DrawingPage implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
-    console.log("started")
+    console.log('started');
 
-    let canvas = <HTMLCanvasElement>document.getElementById("canvas")
-    canvas.height = window.innerHeight
-    canvas.width = window.innerWidth
-    let ctx = canvas.getContext("2d")
-    ctx.lineWidth = 5
+    let canvas = <HTMLCanvasElement>document.getElementById('canvas');
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    let ctx = canvas.getContext('2d');
+    ctx.lineWidth = 5;
 
-    let prevX = null
-    let prevY = null
+    let prevX = null;
+    let prevY = null;
 
-    let draw = false
+    let draw = false;
 
-    let clrs:any = document.querySelectorAll(".clr")
-    clrs = Array.from(clrs)
-    clrs.forEach(clr => {
-        clr.addEventListener("click", () => {
-            ctx.strokeStyle = clr.dataset.clr
-        })
-    })
+    let clrs: any = document.querySelectorAll('.clr');
+    clrs = Array.from(clrs);
+    clrs.forEach((clr) => {
+      clr.addEventListener('click', () => {
+        ctx.strokeStyle = clr.dataset.clr;
+      });
+    });
 
-    let clearBtn = document.querySelector(".clear")
-    clearBtn.addEventListener("click", () => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-    })
+    let clearBtn = document.querySelector('.clear');
+    clearBtn.addEventListener('click', () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    });
 
-    let saveBtn = document.querySelector(".save")
-    saveBtn.addEventListener("click", () => {
-        let data = canvas.toDataURL("imag/png")
-        let a = document.createElement("a")
-        a.href = data
-        a.download = "sketch.png"
-        a.click()
-    })
+    let saveBtn = document.querySelector('.save');
+    saveBtn.addEventListener('click', () => {
+      let data = canvas.toDataURL('imag/png');
+      let a = document.createElement('a');
+      a.href = data;
+      a.download = 'sketch.png';
+      a.click();
+    });
 
-    window.addEventListener("mousedown", (e) => draw = true)
-    window.addEventListener("mouseup", (e) => draw = false)
+    window.addEventListener('mousedown', (e) => (draw = true));
+    window.addEventListener('mouseup', (e) => (draw = false));
 
-    window.addEventListener("mousemove", function(e){
-        if(prevX == null || prevY == null || !draw){
-            prevX = e.clientX
-            prevY = e.clientY
-            return
-        }
+    window.addEventListener('mousemove', function (e) {
+      if (prevX == null || prevY == null || !draw) {
+        prevX = e.clientX;
+        prevY = e.clientY;
+        return;
+      }
 
-        let mouseX = e.clientX
-        let mouseY = e.clientY
-        ctx.beginPath()
-        ctx.moveTo(prevX, prevY)
-        ctx.lineTo(mouseX, mouseY)
-        ctx.stroke()
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+      ctx.beginPath();
+      ctx.moveTo(prevX, prevY);
+      ctx.lineTo(mouseX, mouseY);
+      ctx.stroke();
 
-        prevX = e.clientX
-        prevY = e.clientY
-    })
+      prevX = e.clientX;
+      prevY = e.clientY;
+    });
   }
-
 }
